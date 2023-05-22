5ac5b2eca280c16384db68509d4be868a952e95b
Junkvida <129019458+Junkvida@users.noreply.github.com>
2023-05-22 18:19:48 +0900

    Update sitting.java

--- sitting.java
+++ sitting.java
@@ -7,9 +7,15 @@ public class sitting {
         String url = "https://library.donga.ac.kr/libaray-services/facilities/reading-room/";
         Document doc = Jsoup.connect(url).get();
 
-        //Elements e1 = doc.select("tr");
-        //Elements e2 = e1.select("td");
-        doc.text();
-        //System.out.println(e1);
+        Elements e1 = doc.select("thead");
+        System.out.println(e1.text());
+        
+        Elements e2 = doc.getElementsByClass("align-center");
+        for (Element e3 : e2) {
+            System.out.println(e3);
+        }
+        /*
+        tbody - tr - td - a 
+        */
     }
 }
